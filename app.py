c1ca211677528b83b036d338f0643a3938fa0ae2
Akifumi Abe <akiabe0831@gmail.com>
2021-01-02 20:54:59 +0900

    update

--- app.py
+++ app.py
@@ -1,19 +1,1 @@
-from flask import Flask, jsonify
-import joblib
-import pandas as pd
 
-app = Flask(__name__)
-
-
-@app.route('/predict', methods=["POST"])
-def predict():
-    json_ = request.json
-    query_df = pd.DataFrame(json_)
-    preds = clf.predict(query_df)
-    return jsonify({'prediction': list(preds)})
-
-
-if __name__ == '__main__':
-    clf = joblib.load("model_0.bin")
-    clf_columns = joblib.load('clf_columns.bin')
-    app.run(port=8080)
