5b9bfc3ce4b4ad5c9a8509b94c5b87fd6ac88a2a
Akifumi Abe <akiabe0831@gmail.com>
2021-01-02 14:27:38 +0900

    update

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+import joblib
+import pandas as pd
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/predict', methods=["POST"])
+def predict():
+    json_ = request.json
+    query_df = pd.DataFrame(json_)
+    preds = clf.predict(query_df)
+    return jsonify({'prediction': list(preds)})
 
 
 if __name__ == '__main__':
-    app.run()
+    clf = joblib.load("model_0.bin")
+    clf_columns = joblib.load('clf_columns.bin')
+    app.run(port=8080)
